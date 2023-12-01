5ae96d99cb1d894963eb05d07d0d51c3999890d6
Nerijus <136558243+thenerka1@users.noreply.github.com>
2023-12-01 14:38:39 +0200

    Update BaseRabbitMqCommand.php

--- Command/BaseRabbitMqCommand.php
+++ Command/BaseRabbitMqCommand.php
@@ -2,20 +2,14 @@
 
 namespace OldSound\RabbitMqBundle\Command;
 
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Console\Command\Command;
 
-abstract class BaseRabbitMqCommand extends Command implements ContainerAwareInterface
+abstract class BaseRabbitMqCommand extends Command
 {
-    /**
-     * @var ContainerInterface
-     */
-    protected $container;
-
-    public function setContainer(ContainerInterface $container = null): void
-    {
-        $this->container = $container;
+    public function __construct(
+        protected ContainerInterface $container,
+    ) {
     }
 
     /**
